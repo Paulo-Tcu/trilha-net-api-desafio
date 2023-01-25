d88bbb58243c62357cac3f6d946981ec4a3ad84c
Paulo Cesar Querino Da Cruz De Araujo <paulo.cruz@linx.com.br>
2023-01-25 01:16:59 -0300

    concluido

--- Controllers/TarefaController.cs
+++ Controllers/TarefaController.cs
@@ -21,14 +21,24 @@ namespace TrilhaApiDesafio.Controllers
             // TODO: Buscar o Id no banco utilizando o EF
             // TODO: Validar o tipo de retorno. Se não encontrar a tarefa, retornar NotFound,
             // caso contrário retornar OK com a tarefa encontrada
-            return Ok();
+            var tarefa = _context.Tarefas.Find(id);
+
+            if(tarefa == null)
+                return NotFound(new{Erro = "Id não encontrado",Status = 404});
+
+            return Ok(tarefa);
         }
 
         [HttpGet("ObterTodos")]
         public IActionResult ObterTodos()
         {
             // TODO: Buscar todas as tarefas no banco utilizando o EF
-            return Ok();
+            var listTarefas = _context.Tarefas.ToList();
+
+            if(listTarefas == null)
+                return NotFound();
+
+            return Ok(listTarefas);
         }
 
         [HttpGet("ObterPorTitulo")]
@@ -36,14 +46,34 @@ namespace TrilhaApiDesafio.Controllers
         {
             // TODO: Buscar  as tarefas no banco utilizando o EF, que contenha o titulo recebido por parâmetro
             // Dica: Usar como exemplo o endpoint ObterPorData
-            return Ok();
+            var listTarefas = _context.Tarefas.Where(w => w.Titulo == titulo).ToList();
+
+            if(listTarefas == null || listTarefas.Count() == 0)
+                return NotFound(new{Erro="Sem registros para titulo selecionado", Status = 404});
+
+            return Ok(listTarefas);
+        }
+
+        [HttpGet("ObterPorLikeTitulo")]
+        public IActionResult ObterPorLikeTitulo(string titulo)
+        {
+            var listTarefas = _context.Tarefas.Where(x => x.Titulo.ToUpper().Contains(titulo.ToUpper()));
+
+            if(listTarefas == null || listTarefas.Count() == 0)
+                return NotFound();
+
+            return Ok(listTarefas);
         }
 
         [HttpGet("ObterPorData")]
         public IActionResult ObterPorData(DateTime data)
         {
-            var tarefa = _context.Tarefas.Where(x => x.Data.Date == data.Date);
-            return Ok(tarefa);
+            var listTarefas = _context.Tarefas.Where(x => x.Data.Date == data.Date);
+
+            if(listTarefas == null || listTarefas.Count() == 0)
+                return NotFound(new{Erro="Sem registros para data selecionada", Status = 404});
+
+            return Ok(listTarefas);
         }
 
         [HttpGet("ObterPorStatus")]
@@ -51,8 +81,12 @@ namespace TrilhaApiDesafio.Controllers
         {
             // TODO: Buscar  as tarefas no banco utilizando o EF, que contenha o status recebido por parâmetro
             // Dica: Usar como exemplo o endpoint ObterPorData
-            var tarefa = _context.Tarefas.Where(x => x.Status == status);
-            return Ok(tarefa);
+            var listTarefas = _context.Tarefas.Where(x => x.Status == status);
+            
+            if(listTarefas == null || listTarefas.Count() == 0)
+                return NotFound(new{Erro="Sem registros para status selecionado", Status = 404});
+
+            return Ok(listTarefas);
         }
 
         [HttpPost]
@@ -62,7 +96,15 @@ namespace TrilhaApiDesafio.Controllers
                 return BadRequest(new { Erro = "A data da tarefa não pode ser vazia" });
 
             // TODO: Adicionar a tarefa recebida no EF e salvar as mudanças (save changes)
-            return CreatedAtAction(nameof(ObterPorId), new { id = tarefa.Id }, tarefa);
+            if(ModelState.IsValid)
+            {
+                _context.Tarefas.Add(tarefa);
+                _context.SaveChanges();
+                return CreatedAtAction(nameof(ObterPorId), new { id = tarefa.Id }, tarefa);
+            }
+
+            return BadRequest(new{Erro = "Parametros invalidos"});
+
         }
 
         [HttpPut("{id}")]
@@ -71,14 +113,22 @@ namespace TrilhaApiDesafio.Controllers
             var tarefaBanco = _context.Tarefas.Find(id);
 
             if (tarefaBanco == null)
-                return NotFound();
+                return NotFound(new{Erro = "Id não encontrado"});
 
             if (tarefa.Data == DateTime.MinValue)
                 return BadRequest(new { Erro = "A data da tarefa não pode ser vazia" });
 
             // TODO: Atualizar as informações da variável tarefaBanco com a tarefa recebida via parâmetro
             // TODO: Atualizar a variável tarefaBanco no EF e salvar as mudanças (save changes)
-            return Ok();
+            tarefaBanco.Data = tarefa.Data;
+            tarefaBanco.Descricao = tarefa.Descricao;
+            tarefaBanco.Status = tarefa.Status;
+            tarefaBanco.Titulo = tarefa.Titulo;
+
+            _context.Tarefas.Update(tarefaBanco);
+            _context.SaveChanges();
+
+            return Ok(new{id= tarefaBanco.Id, Status = "Atualizado"});
         }
 
         [HttpDelete("{id}")]
@@ -87,9 +137,12 @@ namespace TrilhaApiDesafio.Controllers
             var tarefaBanco = _context.Tarefas.Find(id);
 
             if (tarefaBanco == null)
-                return NotFound();
+                return NotFound(new{Erro = "Id não encontrado"});
 
             // TODO: Remover a tarefa encontrada através do EF e salvar as mudanças (save changes)
+            _context.Tarefas.Remove(tarefaBanco);
+            _context.SaveChanges();
+
             return NoContent();
         }
     }
--- Models/Tarefa.cs
+++ Models/Tarefa.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,17 @@ namespace TrilhaApiDesafio.Models
     public class Tarefa
     {
         public int Id { get; set; }
+
+        [Required]
         public string Titulo { get; set; }
+
+        [Required]
         public string Descricao { get; set; }
+
+        [Required]
         public DateTime Data { get; set; }
+        
+        [Required]
         public EnumStatusTarefa Status { get; set; }
     }
 }
